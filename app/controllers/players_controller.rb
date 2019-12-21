e268304cab822c152fcd1a586d5ab0b8a3370d32
github username <github email address>
2019-12-21 16:52:36 -0600

    create new user form and post to API

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,15 @@
 class PlayersController < ApplicationController
- 
+  def index
+    players = Player.all
+    render json: players
+  end 
+
+  def create
+    @new = Player.create(player_params)
+    puts @new
+end
+
+def player_params
+  params.require(:player).permit(:username, :cohort)
+end
 end
