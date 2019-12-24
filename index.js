ce037fde973f4d32e5cd3637f98dc19d00fb3754
github username <github email address>
2019-12-24 13:52:23 -0600

    passing username to home page

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+  let existingSubmitButton = document.querySelector('#submit-existing')
   let newSubmitButton = document.querySelector('#submit-new')
   let cohortSelect = document.querySelector('#existing-cohort')
+  let userForms = document.querySelectorAll(".user-form");
+  
+  
 
   let playerMatchesURL = 'http://localhost:3000/player_matches'
   let playersURL = 'http://localhost:3000/players'
@@ -28,6 +32,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
   // getMatches()
   // getPlayerMatches()
 
+  // function getUserForms() {
+  //   window.onload = function() {
+  //     userForms.forEach(form => {
+  //       form.style.display = "block";
+        
+  //     })
+  //   }}
+
   
   newSubmitButton.addEventListener('click', e => {
     e.preventDefault();
@@ -38,34 +50,76 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let username = `${firstName}_${lastName}`
     console.log(username, cohort)   
     
+    if (firstName && lastName){
     let data = {
       username: username,
       cohort: cohort
      }
+     postFetch(playersURL, data)
+    } else {
+      console.log(`no info`)
+    }
 
-     fetch(playersURL, {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-         'Accept': 'application/json'
-       },
-         body: JSON.stringify(data)
-     }) 
+    removeForms(userForms)
+    goToMenu(username)
+     
   }) 
+
+  existingSubmitButton.addEventListener('click', e => {
+    e.preventDefault()
+    let studentDropdown = document.getElementById('existing-students');
+    var username = studentDropdown.options[studentDropdown.selectedIndex].innerText;
+    
+    removeForms(userForms)
+    goToMenu(username)
+
+  })
   
   cohortSelect.onchange = function() {
     let cohortName = document.querySelector('#existing-cohort').value
     let studentDropdown = document.querySelector('#existing-students')
     studentDropdown.innerHTML = ''
+    i = 0
     fetch(playersURL)
     .then(res => res.json())
     .then(data => data.forEach(player => {
+      
       if (player.cohort == cohortName){
         let option = document.createElement('option')
+        option.setAttribute('value', `${i}`)
         option.innerText = player.username
         studentDropdown.appendChild(option)
+        i ++
+        console.log(option)
       }
     }));
   }
+
+  function goToMenu(username){
+    let greetUser = document.querySelector('.logged-in-user')
+    let createButton = document.querySelector('.create-match')
+    greetUser.innerHTML = `<u>Welcome, ${username}!</u>`
+    createButton.style.display = 'block';
+  }
+
+  
+  // <---------------Helper Methods------------->
+  function postFetch(url, data){
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+      body: JSON.stringify(data)
+    }) 
+  }
+
+  function removeForms(forms){
+    forms.forEach(form => {
+      form.style.display = "none";
+    })
+  }
+  //  getUserForms()
 
 })
