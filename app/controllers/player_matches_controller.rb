c4149806d4e4af0c44152bb89b436ceb852f859b
github username <github email address>
2019-12-26 14:22:12 -0600

    created player_match

--- app/controllers/player_matches_controller.rb
+++ app/controllers/player_matches_controller.rb
@@ -3,4 +3,13 @@ class PlayerMatchesController < ApplicationController
     playerMatches = PlayerMatch.all
     render json: playerMatches, include: [ :player_1, :player_2, :match]
   end 
+
+  def create
+    @new = PlayerMatch.create(playerMatch_params)
+    puts @new
+  end
+
+def playerMatch_params
+  params.require(:player_match).permit(:player_1_id, :player_2_id, :match_id)
+end
 end
