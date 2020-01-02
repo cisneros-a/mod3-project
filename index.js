0016ea0731420bf0e19f64dd58c474603c3d80ca
Buford Eeds <bufordeeds8@gmail.com>
2020-01-02 15:00:24 -0600

    fiex user flow

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
   let playersURL = 'http://localhost:3000/players'
   let matchesURL = 'http://localhost:3000/matches'
   let matchData
-
+  let statsContainer = document.querySelector('#stats__container')
+  let currentMatch = document.querySelector('#current_match')
   // clearGameInfo()
 
   const navLinks = document.querySelectorAll('.nav-link')
@@ -119,9 +120,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let createMatchButton = document.querySelector('.create-match')
     let joinMatchButton = document.querySelector('.join-match')
 
-    // greetUser.innerHTML = `Welcome, ${username}!`
-    // actually, you can't right now. Not by classname
-    // right. 
+    
     createMatchButton.addEventListener('click', e => {
       let data = {
         winner_id: null,
@@ -136,6 +135,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     joinMatchButton.addEventListener('click', e => {
       showExistingMatches(username, playersData, keepscore)
     })
+
+    statsContainer.classList.add('show-view')
 
   }
 
@@ -196,7 +197,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
               loggedInId: logged_in.id,
               matchId: match.id
             }
-
+            // currentMatch.classList.add('show-view')
             // greetUser.innerHTML = `You have joined ${hostName}'s match!`
             callback()
           })
@@ -205,6 +206,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 
   let keepscore = () => {
+    currentMatch.classList.add('show-view')
     p1WinCount = 0
     p2WinCount = 0
     p1WinCountDiv.innerText = `${matchData.hostUsername}'s win count: ${p1WinCount}`
